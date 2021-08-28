349ccaffb027ab0691029c16b7552e4cda300340
biddls <thomasbiddlecombe@gmail.com>
2021-08-28 01:41:41 +0100

    bby steps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import Web3 from 'web3'
 import './App.css';
-import { useState } from 'react';
-import { ethers } from 'ethers';
+// import { useState } from 'react';
+// import { ethers } from 'ethers';
 
 import daoNFTsABI from "./artifacts/@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol/ERC1155PresetMinterPauser.json"
 import mapABI from "./artifacts/contracts/ALCX_map.sol/ALCX_map.json"
 import mapNFTsABI from "./artifacts/@openzeppelin/contracts/token/ERC721/ERC721.sol/ERC721.json"
 
-const Map_Addr = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
-const MapNFT_Addr = "0x75537828f2ce51be7289709686A69CbFDbB714F1"
-const alcDAO_Addr = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
-
-
+const Map_Addr = "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+const MapNFT_Addr = "0x3B02fF1e626Ed7a8fd6eC5299e2C54e1421B626B"
+const alcDAO_Addr = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"
 class App extends Component {
 
     async componentWillMount() {
@@ -41,19 +39,32 @@ class App extends Component {
             const map = new web3.eth.Contract(mapABI.abi, Map_Addr)
             const mapNFTs = new web3.eth.Contract(mapNFTsABI.abi, MapNFT_Addr)
             const daoNFTs = new web3.eth.Contract(daoNFTsABI.abi, alcDAO_Addr)
-            this.setState({ map })
-            this.setState({ mapNFTs })
-            this.setState({ daoNFTs })
-            await this.loadContractData()
+            this.setState({
+                mapCont: map,
+                mapNFTs: mapNFTs,
+                daoNFTs: daoNFTs,
+                mapNextX: await map.methods.nextX().call(),
+                mapNextY: await map.methods.nextY().call(),
+                radius: await map.methods.radius().call()})
+            let tempMap = await this.createMap(5, () => null)// need to sort
+
+            console.log(await this.state.mapCont.methods.map(0, 0))
+
+            // for(let x = 0;x<this.state.radius;x++){
+            //     for(let y = 0;y<this.state.radius;y++){
+            //         tempMap[x][y] = await this.state.mapCont.map.call()
+            //     }
+            // }
+            // console.log(tempMap)
         } else {
             window.alert('Smart contract not deployed to detected network.')
         }
     }
 
-    async loadContractData (){
-        this.setState({ mapNextX: await this.map.methods.nextX.call() })
-        this.setState({ mapNextY: await this.map.methods.nextY.call() })
-
+    async createMap(radius, mapper) {
+        return Array(radius)
+            .fill()
+            .map(() => Array(radius).fill().map(mapper))
     }
 
     async getTile(x, y) {
@@ -76,28 +87,23 @@ class App extends Component {
         super(props)
         this.state = {
             account: '',
-            map: null,
-            mapNFTs: null,
+            mapCont: null,
+            mapNFTsCont: null,
             daoNFTs: null,
-            mapNextX: 0,
-            mapNextY: 1,
+            mapNextX: null,
+            mapNextY: null,
+            radius: null,
+            map: null,
         }
     }
 
     render() {
         return (
             <div>
-                <p>your account {this.state.account}</p>
-                <p>map contract address {Map_Addr}</p>
-                <p>map nft address {MapNFT_Addr}</p>
-                <p>alc dao nft address {alcDAO_Addr}</p>
-                <p></p>
+                {this.state.account} - {this.state.mapNextX} - {this.state.mapNextY} - {this.state.radius}
             </div>
         );
     }
 }
 
 export default App;
-// Map address:  0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
-// Map NFT address:  0x75537828f2ce51be7289709686A69CbFDbB714F1
-// alc DAO address:  0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9
